c69556dbd42ab5c100b678efa7fb96ba1c3335de
Nagma Bano <nagma.bano@sap.com>
2024-01-28 13:43:59 +0530

    running app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.myandroidapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myandroidapp"
